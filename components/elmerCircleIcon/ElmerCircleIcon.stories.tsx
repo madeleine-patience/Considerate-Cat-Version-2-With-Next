a052841c9e19123d9b16bd39f87b8355b98675bb
madeleinepbelanger <madeleinepbelanger@gmail.com>
2024-08-10 10:22:08 -0700

    Elmer story reverted

--- components/elmerCircleIcon/ElmerCircleIcon.stories.tsx
+++ components/elmerCircleIcon/ElmerCircleIcon.stories.tsx
@@ -13,43 +13,42 @@ export default {
   component: ElmerCircleIcon,
 } as Meta<typeof ElmerCircleIcon>;
 
-const states = [
-  {
-    name: "Small",
-    render: () => <ElmerCircleIcon {...props} size="small" />,
-  },
-  {
-    name: "Medium",
-    render: (props: ElmerIconProps) => <ElmerCircleIcon {...props} />,
-  },
-  {
-    name: "Medium",
-    render: (props: ElmerIconProps) => (
-      <ElmerCircleIcon {...props} size="large" />
-    ),
-  },
-];
-const Template: StoryFn<typeof ElmerCircleIcon> = (props) => (
-  <Table sx={{ th: { fontWeight: 700 } }}>
-    <TableHead>
-      <TableRow>
-        <TableCell />
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {states.map(({ name, render }) => (
-        <TableRow key={name}>
-          <TableCell component="th" align="center">
-            {name}
-          </TableCell>
-          <TableCell align="center">{render({ ...props })}</TableCell>
-        </TableRow>
-      ))}
-    </TableBody>
-  </Table>
-);
+// const states = [
+//   {
+//     name: "Small",
+//     render: () => <ElmerCircleIcon {...props} size="small" />,
+//   },
+//   {
+//     name: "Medium",
+//     render: (props: ElmerIconProps) => <ElmerCircleIcon {...props} />,
+//   },
+//   {
+//     name: "Medium",
+//     render: (props: ElmerIconProps) => (
+//       <ElmerCircleIcon {...props} size="large" />
+//     ),
+//   },
+// ];
+// const Template: StoryFn<typeof ElmerCircleIcon> = (props) => (
+//   <Table sx={{ th: { fontWeight: 700 } }}>
+//     <TableHead>
+//       <TableRow>
+//         <TableCell />
+//       </TableRow>
+//     </TableHead>
+//     <TableBody>
+//       {states.map(({ name, render }) => (
+//         <TableRow key={name}>
+//           <TableCell component="th" align="center">
+//             {name}
+//           </TableCell>
+//           <TableCell align="center">{render({ ...props })}</TableCell>
+//         </TableRow>
+//       ))}
+//     </TableBody>
+//   </Table>
+// );
 
 export const Default = {
-  render: Template,
   args: {},
 };
