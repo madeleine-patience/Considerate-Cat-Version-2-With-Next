7c03d2df71dcd93c3693bbc00a953d1ba0e51ca7
madeleinepbelanger <madeleinepbelanger@gmail.com>
2024-08-11 13:39:54 -0700

    Disabling greens

--- components/homepageWelcome/HomepageWelcome.tsx
+++ components/homepageWelcome/HomepageWelcome.tsx
@@ -43,7 +43,7 @@ const HomepageWelcome = ({ sx }: HomepageWelcomeProps) => {
         borderRadius: 3,
         boxShadow:
           "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;",
-        backgroundColor: palette.greens.light,
+        // backgroundColor: palette.greens.light,
         ...sx,
       }}
     >
