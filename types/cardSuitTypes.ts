a7e91e6316ebf6399fce9fb4f0ed737af4f9b204
Bryan <54081465+arthvadrr@users.noreply.github.com>
2024-10-23 01:02:42 -0500

    chore: Removed the types dir

--- types/cardSuitTypes.ts
+++
@@ -1,1 +0,0 @@
-export type CardSuitTypes = 'Major' | 'Cups' | 'Pentacles' | 'Swords' | 'Wands';
--- types/tarotDeckData.ts
+++
@@ -1,8 +0,0 @@
-export type TarotDeckData = {
-  id: number;
-  card_name: string;
-  card_suit: string;
-  description: string;
-  image_link: string;
-  key_words: string[];
-};
