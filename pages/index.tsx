ad72564bb8706eee8f9cdc62a67866c17cec62ce
Bryan <54081465+arthvadrr@users.noreply.github.com>
2024-10-26 18:15:59 -0500

    chore: Added alt text to component images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import { Box, useMediaQuery, useTheme } from '@mui/material';
-import FlowerFooter from '../components/flowerFooter/FlowerFooter';
-import Header from '../components/header/Header';
-import HomepageWelcome from '../components/homepageWelcome/HomepageWelcome';
 import { LoadingPage } from '../components/loadingPage/LoadingPage';
 import { MagicCatReload } from '../components/magicCatAnimation/magicCatReload/MagicCatReload';
 import { TarotDeckContext } from '../context/TarotDeckContext';
-import useFetchTarotDeck from '../hooks/fetchTarotDeck';
 import { useTarotCard } from '../hooks/useTarotCard';
+import FlowerFooter from '../components/flowerFooter/FlowerFooter';
+import Header from '../components/header/Header';
+import HomepageWelcome from '../components/homepageWelcome/HomepageWelcome';
+import useFetchTarotDeck from '../hooks/fetchTarotDeck';
 import type { Theme } from '@mui/system';
 import type { ReactElement } from 'react';
 
@@ -39,6 +39,7 @@ export default function Home(): ReactElement {
             <Box
               component='img'
               src={homePageMainImage}
+              alt='A whimsical illustration of a friendly cat seated at a kitchen table, performing a tarot reading from the Considerate Cat Tarot deck with a glass of hot tea nearby. The scene is framed by a cozy kitchen, with a lovely night sky visible through a window in the background.'
               width='100%'
               sx={{
                 display: isSmallScreen ? 'none' : undefined,
