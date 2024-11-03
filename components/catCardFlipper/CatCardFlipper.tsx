c48e244731dc108cadfa30416ab414563492d912
madeleinepbelanger <madeleinepbelanger@gmail.com>
2024-11-03 12:17:09 -0800

    Re-arranged files to extract homepage welcome and delete unused component

---
+++ components/catCardFlipper/CatCardFlipper.tsx
@@ -0,0 +1,86 @@
+import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { useState } from 'react';
+import SingleTarotCard from '../singleTarotCardWithFlip/SingleTarotCardWithFlip';
+
+const CatCardFlipper = () => {
+  const theme = useTheme();
+  const [petCount, setPetCount] = useState(0);
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [butters, setButters] = useState(
+    `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/considerate%20cat%20assets/No-Background-Butters-1.png`
+  );
+
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const petTheCat = () => {
+    setPetCount((prev) => prev + 1);
+
+    if (petCount >= 0) {
+      setButters(
+        `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/considerate%20cat%20assets/No-Background-Butters-2.png`
+      );
+      setIsFlipped(true);
+    }
+  };
+  /**
+   * TODO Replace components with role="button"
+   */
+  return (
+    <Box
+      onClick={() => petTheCat()}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+          e.preventDefault();
+          petTheCat();
+        }
+      }}
+      role='button'
+      tabIndex={0}
+      sx={{ width: 'fit-content' }}
+    >
+      <Box
+        component='img'
+        src={butters}
+        alt='Butters the cat, waiting to show you a tarot reading if you give pets.'
+        sx={{
+          top: 25,
+          zIndex: 2,
+          position: 'relative',
+          width: isSmallScreen ? '100%' : '500px',
+          filter:
+            ' drop-shadow(9px 0 0 white) drop-shadow(0 9px 0 white) drop-shadow(-9px 0 0 white) drop-shadow(0 -9px 0 white)'
+        }}
+      />
+      <Box
+        sx={{
+          zIndex: 1,
+          position: 'relative',
+          display: 'flex',
+          justifyContent: 'center',
+          gap: 2,
+          top: -50
+        }}
+      >
+        <SingleTarotCard
+          image='/image/welcome-cards/three-of-pentacles.webp'
+          altText='A tarot card with the text, “Three of Pentacles.” The artwork depicts a green-walled room with a view of a tree, a table with an artist’s palette, and two cats—one playing with the paint-covered palette, and another napping on the chair. Three sheets of painted pentacles are on the desk.'
+          isCardFlipped={isFlipped}
+          size='small'
+        />
+        <SingleTarotCard
+          image='/image/welcome-cards/the-emperor.webp'
+          altText='A tarot card with the text, “The Emperor.” The artwork depicts a cat wearing a large crown.'
+          isCardFlipped={isFlipped}
+          size='small'
+        />
+        <SingleTarotCard
+          image='/image/welcome-cards/three-of-cups.webp'
+          altText='A tarot card with the text, “Three of Cups.” The artwork depicts two friendly cats wearing birthday hats, leaning on each other at a table with waffles and strawberries. Three wine glasses are on the table.'
+          isCardFlipped={isFlipped}
+          size='small'
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default CatCardFlipper;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,21 @@
-import { Box, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import type { Theme } from '@mui/system';
+import { useRouter } from 'next/router';
+import type { ReactElement } from 'react';
+import CatCardFlipper from '../components/catCardFlipper/CatCardFlipper';
+import FlowerFooter from '../components/flowerFooter/FlowerFooter';
 import { Header } from '../components/header';
 import { LoadingPage } from '../components/loadingPage/LoadingPage';
 import { MagicCatReload } from '../components/magicCatAnimation/magicCatReload/MagicCatReload';
+import RaisedButton from '../components/raisedButton/RaisedButton';
 import { TarotDeckContext } from '../context/TarotDeckContext';
-import { useTarotCard } from '../hooks/useTarotCard';
-import FlowerFooter from '../components/flowerFooter/FlowerFooter';
-import HomepageWelcome from '../components/homepageWelcome/HomepageWelcome';
 import useFetchTarotDeck from '../hooks/fetchTarotDeck';
-import type { Theme } from '@mui/system';
-import type { ReactElement } from 'react';
+import { useTarotCard } from '../hooks/useTarotCard';
 
 const homePageMainImage = '/image/homepageImage.webp';
 
 export default function Home(): ReactElement {
+  const router = useRouter();
   const { tarotDeck, loading } = useFetchTarotDeck();
   const { tarotCardData } = useTarotCard(tarotDeck);
   const { palette } = useTheme();
@@ -55,7 +58,54 @@ export default function Home(): ReactElement {
               }}
             />
           </Box>
-          <HomepageWelcome />
+          <Box
+            sx={{
+              p: 4,
+              borderRadius: 3
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'alignItems',
+                alignContent: 'center',
+                alignItems: 'center',
+                flexDirection: isSmallScreen ? 'column' : 'row'
+              }}
+            >
+              <CatCardFlipper />
+              <Box
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  gap: 4,
+                  textAlign: 'center',
+                  justifyContent: 'center',
+                  alignItems: 'center'
+                }}
+              >
+                <Typography variant='h6'>
+                  Considerate Cat is a tarot deck that was published in 2018,
+                  brought to life by a community of folks on Kickstarter. Since
+                  then, the deck has been brought into many inspiring
+                  people&apos;s homes, featuring stories of rescue cats from
+                  across the world. Now, we are bringing the deck to life
+                  online! Here, you can have your cards read, learn about the
+                  cats that inspired the deck, and dive deeper into the artwork
+                </Typography>
+                <Typography variant='h6'>
+                  Also, Butters is patiently waiting to have his cards read.
+                  Click him to give him the reading he&apos;s been waiting for!
+                </Typography>
+                <RaisedButton
+                  buttonLabel='Get My Own Tarot Read!'
+                  onClick={() => {
+                    router.push('/TarotReadPage');
+                  }}
+                />
+              </Box>
+            </Box>
+          </Box>
         </Box>
         <FlowerFooter />
       </Box>
