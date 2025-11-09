5eeb0d6ff8dd4fe00fdc52a6163430fe2774e388
madeleinepbelanger <madeleinepbelanger@gmail.com>
2025-11-09 12:07:43 -0800

    Added connection with supabase

--- components/emailSignupModal/emailSignup.form.ts
+++ components/emailSignupModal/emailSignup.form.ts
@@ -1,31 +1,42 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import {
+  EMAIL_SELECTION,
   EmailChoiceSchema,
   emailChoiceValidationSchema
 } from '../../schemas/userEmailSchema';
+
+type EmailMarketingType =
+  | 'marketingInformationOnly'
+  | 'openSourceInformationOnly'
+  | 'allTypesOfEmails';
+export interface EmailFormProps {
+  Email: string;
+  Name: string;
+  EmailChoice: EmailMarketingType;
+}
 
 export function useEmailMarketingForm() {
   const form = useForm<EmailChoiceSchema>({
     defaultValues: {
       Name: '',
       Email: '',
-      EmailChoice: 'marketing'
+      EmailChoice: EMAIL_SELECTION.MARKETING
     },
     resolver: zodResolver(emailChoiceValidationSchema)
   });
 
-  const { control, handleSubmit, formState, reset } = form;
-
-  const handleFormSubmit = handleSubmit((values) => {
-    // TODO : Remove in part two
-    console.log('Form was submitted, values:', values);
-  });
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { isSubmitSuccessful }
+  } = form;
 
   return {
     control,
-    handleFormSubmit,
+    handleSubmit,
     reset,
-    formState
+    isSubmitSuccessful
   };
 }
